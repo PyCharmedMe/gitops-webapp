acaadfbf1e1964102fa1fe8ec16bb5fd402d9ca8
PyCharmedMe <pikachu763@gmail.com>
2026-02-05 22:49:03 +0200

    console log check

--- app/frontend/app.js
+++ app/frontend/app.js
@@ -1,7 +1,10 @@
 function callBackend() {
+  console.log("Button clicked");
   fetch("http://192.168.49.2:32694/api/hello")
     .then(res => res.json())
     .then(data => {
+      console.log(data);
       document.getElementById("result").innerText = data.message;
-    });
+    })
+    .catch(err => console.error("Fetch error:", err));
 }
